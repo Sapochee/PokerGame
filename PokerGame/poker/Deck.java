026a4885625a7f3ae910bff72c2cb3aafe453cec
Sapochee <129986526+Sapochee@users.noreply.github.com>
2023-10-10 13:11:25 -0400

    Add files via upload

---
+++ PokerGame/poker/Deck.java
@@ -0,0 +1,42 @@
+package poker;
+
+public class Deck {
+
+	private Card[] cards;
+
+	public Deck() {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+
+	public Deck(Deck other) {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+
+	public Card getCardAt(int position) {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+
+	public int getNumCards() {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+
+	public void shuffle() {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+
+	public void cut(int position) {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+
+	public Card[] deal(int numCards) {
+		/* Remove the statement below and implement this method */
+		throw new UnsupportedOperationException();
+	}
+		
+}
---
+++ PokerGame/poker/StudentTests.java
@@ -0,0 +1,17 @@
+package poker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudentTests {
+
+	/* Use the @Test annotation for JUnit 4 
+	 * [This is just an example, please erase
+	 * it and write some real tests.]    */
+	@Test
+	public void test() {
+		
+	}
+
+}
